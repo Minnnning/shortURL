ee2d929cbc1974d71bcaa35383269004012f0df2
minjeong <6alswjd6@gmail.com>
2023-07-26 10:33:35 +0900

    add url storage

--- shorten.py
+++ shorten.py
@@ -1,8 +1,9 @@
-import config
+# import config
 import randstr
-import os
+# import os
 
 def shorten_url(url):
-    token = randstr.generate_rand_token(6)
-    print(token)
-    return os.path.join(config.host,token)
+#EDIT :  token에 host url을 직접 붙여서 리턴하는 방식에서 token만 리턴
+#  token = randstr.generate_rand_token(6)
+#  return os.path.join(config.host,token)
+    return randstr.generate_rand_token(6)
